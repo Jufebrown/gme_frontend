37bb632717c3148745397c0632ff35dad74d755b
JufeBrown <jufebrown@gmail.com>
2025-06-05 09:11:57 -0500

    changed the chart title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GMEChart from "@/components/GMEChart";
 export default function Home() {
   return (
     <main>
-      <h1>GME Price (Last 7 Days)</h1>
+      <h1>GME Time Series</h1>
       <GMEChart />
     </main>
   );
